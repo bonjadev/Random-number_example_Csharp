3c92b99846c2d07ec43fa7879eb26cf3d745e61c
Bojan B <83359157+bonjadev@users.noreply.github.com>
2023-04-29 07:28:26 +0200

    First commit.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Random_number
+{
+    class Program
+    {
+        //Create a C# program that prompts the user for 2 integers and displays a random number on the screen between those values.
+
+        static void Main(string[] args)
+        {
+            int n1 = int.Parse(Console.ReadLine());
+            int n2 = int.Parse(Console.ReadLine());
+
+            Random rnd = new Random();
+            int number = rnd.Next(n1, n2 + 1);
+
+            Console.WriteLine(number);
+        }
+    }
+}
